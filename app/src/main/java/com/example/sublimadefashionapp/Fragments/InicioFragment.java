02a513a84b7819a04a36b70a0540e1bb837ee253
jesusalcalaluna <jesusalcalaluna@users.noreply.github.com>
2019-03-18 19:51:37 -0600

    detalle

--- app/src/main/java/com/example/sublimadefashionapp/Fragments/InicioFragment.java
+++ app/src/main/java/com/example/sublimadefashionapp/Fragments/InicioFragment.java
@@ -44,10 +44,10 @@ public class InicioFragment extends Fragment {
      * @return A new instance of fragment InicioFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static InicioFragment newInstance(String id, String param2) {
+    public static InicioFragment newInstance(String param1, String param2) {
         InicioFragment fragment = new InicioFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, id);
+        args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
