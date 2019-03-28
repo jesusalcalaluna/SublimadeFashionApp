9e9d6de344f59c125f26add8182dc54a2dc6ae14
Aguirrido <45271970+Aguirrido@users.noreply.github.com>
2019-03-28 16:02:49 -0600

    Funcionando registrar personas y usuarios

--- app/src/main/java/com/example/sublimadefashionapp/login.java
+++ app/src/main/java/com/example/sublimadefashionapp/login.java
@@ -1,25 +1,158 @@
 package com.example.sublimadefashionapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class login extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
+
+public class login extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
+    private GoogleApiClient googleApiClient;
+    private TextView signInButton, as;
+    public static final int SIGN_IN_CODE=777;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        GoogleSignInOptions gso = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestProfile()
+                .requestEmail()
+                .requestIdToken(getString(R.string.token)).build();
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
 
+        signInButton = findViewById(R.id.signInButton);
+        signInButton.setOnClickListener(this);
+
+        as = findViewById(R.id.as);
+        as.setOnClickListener(this);
+
+
+        firebaseAuth= FirebaseAuth.getInstance();
+        authStateListener= new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user!=null){
+
+                    //metodo que lleva a activity home una vez se haya logeado
+                    goMainScreen();
+                    startActivity(new Intent(login.this, MainActivity.class));
+                }
+                else
+                {
+
+
+                }
+            }
+        };
+
+    }
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+
+            case R.id.signInButton:
+                Intent signInIntent =Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(signInIntent,SIGN_IN_CODE);
+                break;
+            case R.id.as:
+                Intent intent = new Intent(login.this,Register.class);
+                startActivity(intent);
+                break;
+        }
 
     }
 
-    public void intntregistrar(View view) {
-        Intent sig = new Intent(this, Register.class);
-        startActivity(sig);
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+    @Override
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //Checa respuesta de logeo de Google
+        if (requestCode == SIGN_IN_CODE){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            HandleSignResult(result);
+        }
+    }
+
+
+    private void HandleSignResult(GoogleSignInResult result) {
+        if(result.isSuccess()){
+            firebaseAuthWithGoogle(result.getSignInAccount());
+        } else{
+            Toast.makeText(this,"nose puede iniciar sesion",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount signInAccount) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(signInAccount.getIdToken(),null);
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if ( !task.isSuccessful() )
+                {
+                    Toast.makeText(login.this, "No se pudo autenticar ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void goMainScreen() {
+        Intent intent = new Intent(this,MainActivity.class);
+        //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(authStateListener!=null){
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
+    }
+
+    public void iniciar(View view) {
+        Intent intent = new Intent(this,login.class);
+        startActivity(intent);
+        finish();
     }
 }
